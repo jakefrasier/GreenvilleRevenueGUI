c2fc8e924dee3444548baad1b9676b0ee3fbcd14
Erik Johnson <sgtarmy@aol.com>
2017-02-07 14:51:54 -0600

    Initial Commit

---
+++ GreenvilleRevenueGUI/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace GreenvilleRevenueGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastYearTextBox = new System.Windows.Forms.TextBox();
+            this.thisYearTextBox = new System.Windows.Forms.TextBox();
+            this.expectedRevenueLabel = new System.Windows.Forms.Label();
+            this.greaterLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the number of contestants last year:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter the number of contestants this year:";
+            // 
+            // lastYearTextBox
+            // 
+            this.lastYearTextBox.Location = new System.Drawing.Point(264, 16);
+            this.lastYearTextBox.Name = "lastYearTextBox";
+            this.lastYearTextBox.Size = new System.Drawing.Size(56, 20);
+            this.lastYearTextBox.TabIndex = 3;
+            this.lastYearTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // thisYearTextBox
+            // 
+            this.thisYearTextBox.Location = new System.Drawing.Point(264, 40);
+            this.thisYearTextBox.Name = "thisYearTextBox";
+            this.thisYearTextBox.Size = new System.Drawing.Size(56, 20);
+            this.thisYearTextBox.TabIndex = 4;
+            this.thisYearTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // expectedRevenueLabel
+            // 
+            this.expectedRevenueLabel.AutoSize = true;
+            this.expectedRevenueLabel.Location = new System.Drawing.Point(16, 128);
+            this.expectedRevenueLabel.Name = "expectedRevenueLabel";
+            this.expectedRevenueLabel.Size = new System.Drawing.Size(35, 13);
+            this.expectedRevenueLabel.TabIndex = 5;
+            this.expectedRevenueLabel.Text = "label3";
+            this.expectedRevenueLabel.Visible = false;
+            // 
+            // greaterLabel
+            // 
+            this.greaterLabel.AutoSize = true;
+            this.greaterLabel.Location = new System.Drawing.Point(24, 160);
+            this.greaterLabel.Name = "greaterLabel";
+            this.greaterLabel.Size = new System.Drawing.Size(35, 13);
+            this.greaterLabel.TabIndex = 6;
+            this.greaterLabel.Text = "label3";
+            this.greaterLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 261);
+            this.Controls.Add(this.greaterLabel);
+            this.Controls.Add(this.expectedRevenueLabel);
+            this.Controls.Add(this.thisYearTextBox);
+            this.Controls.Add(this.lastYearTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Greenville Revenue";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lastYearTextBox;
+        private System.Windows.Forms.TextBox thisYearTextBox;
+        private System.Windows.Forms.Label expectedRevenueLabel;
+        private System.Windows.Forms.Label greaterLabel;
+    }
+}
+
---
+++ GreenvilleRevenueGUI/Form1.cs
@@ -0,0 +1,44 @@
+﻿// Program written by: Erik Johnson, Jake Frasier
+//       Program date: 1/28/17
+//Program description:  Greenville Revenue Chapter 3, Case Problem 1
+// Encountered issues: 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenvilleRevenueGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double lastYearContestants, thisYearContestants, expectedRevenue;
+            //string lastYearContestantsAsString, thisYearContestantsAsString;
+            bool moreThanLastYear;
+            
+            lastYearContestants = Convert.ToDouble(lastYearTextBox.Text);
+            thisYearContestants = Convert.ToDouble(thisYearTextBox.Text);
+            expectedRevenue = thisYearContestants * 25; // entrence fee is $25
+            expectedRevenueLabel.Text=("This year's projected income is "+ expectedRevenue.ToString("C"));
+            moreThanLastYear = thisYearContestants > lastYearContestants;
+            greaterLabel.Text="Are there more contestants at this year's event than last year: "+ moreThanLastYear;
+
+            expectedRevenueLabel.Visible = true;
+            greaterLabel.Visible = true;
+
+
+
+        }
+    }
+}
